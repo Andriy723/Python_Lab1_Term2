71be8e6f0e84a2ca00d4375783c62d129a62f5ce
Andriy723 <113835398+Andriy723@users.noreply.github.com>
2023-05-15 11:38:15 +0300

    Some changing in get_instatce

--- ua/lviv/iot/algo/part1/lab1/python/manager/Manager.py
+++ ua/lviv/iot/algo/part1/lab1/python/manager/Manager.py
@@ -1,15 +1,19 @@
 from Python_Lab1_Term2.ua.lviv.iot.algo.part1.lab1.python.model.Trolleybus import Trolleybus
 
-def main():
-   trolleybuses = [
-    Trolleybus(44, 36, 122, 27, 3, 22),
-    Trolleybus(11, 22, 110, 44, 11, 22),
-    # Trolleybus.get_instance(),
-    # Trolleybus.get_instance()
-]
 
-   for trolleybus in trolleybuses:
-      print(trolleybus)
+def main():
+    trolleybuses = [
+        Trolleybus(44, 36, 122, 27, 3, 22),
+        Trolleybus(11, 22, 110, 44, 11, 22),
+        Trolleybus.get_instance(),
+        Trolleybus.get_instance()
+    ]
+
+    print(trolleybuses[::])
+
+    for trolleybus in trolleybuses:
+        print(trolleybus)
+
 
 if __name__ == '__main__':
     main()
--- ua/lviv/iot/algo/part1/lab1/python/model/Trolleybus.py
+++ ua/lviv/iot/algo/part1/lab1/python/model/Trolleybus.py
@@ -1,35 +1,41 @@
 class Trolleybus:
-    def __init__(self, route_number, current_stop, max_speed, capacity, passengers, current_speed, id = 100):
-        self.id = id
+
+    __default_trolleybus = None
+
+    def __init__(self, route_number=None, current_stop=None,
+                 max_speed=None, capacity=None, passengers=None, current_speed=None, id=100):
         self.route_number = route_number
         self.current_stop = current_stop
         self.max_speed = max_speed
         self.capacity = capacity
         self.passengers = passengers
         self.current_speed = current_speed
+        self.id = id
 
     def __str__(self):
         return f"({self.route_number}, {self.current_stop}, {self.max_speed}, {self.capacity}, {self.passengers}," \
                f" {self.current_speed}, {self.id})"
 
-# @staticmethod
-# def get_instance():
-#      if Trolleybus._default_trolleybus is None:
-#         Trolleybus._default_trolleybus = Trolleybus()
-#      return Trolleybus._default_trolleybus
+    @staticmethod
+    def get_instance():
+        if Trolleybus.__default_trolleybus is None:
+            Trolleybus.__default_trolleybus = Trolleybus()
+        return Trolleybus.__default_trolleybus
 
-def stop(self):
-  self.current_speed = 0
+    def stop(self):
+        self.current_speed = 0
 
-def start(self):
- self.current_speed = 20
+    def start(self):
+        self.current_speed = 20
 
-def add_passenger(self):
-    if self.passengers < self.capacity:
-      self.passengers += 1
-    else: print("Trolleybus is overcrowded!")
+    def add_passenger(self):
+        if self.passengers < self.capacity:
+            self.passengers += 1
+        else:
+            print("Trolleybus is overcrowded!")
 
-def remove_passenger(self):
-  if self.passengers > self.capacity:
-       self.passengers -= 1
-  else: print("Trolleybus is not overcrowded! So, you can get into it.")
+    def remove_passenger(self):
+        if self.passengers > self.capacity:
+            self.passengers -= 1
+        else:
+            print("Trolleybus is not overcrowded! So, you can get into it.")
