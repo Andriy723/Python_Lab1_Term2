d564ccb14c1e83c0fd89406c65f567a273df93ee
Andriy723 <113835398+Andriy723@users.noreply.github.com>
2023-05-15 15:30:30 +0300

    Changing the lab and adding some doc strings

--- ua/lviv/iot/algo/part1/lab1/python/manager/manager.py
+++ ua/lviv/iot/algo/part1/lab1/python/manager/manager.py
@@ -1,13 +1,21 @@
-from Python_Lab1_Term2.ua.lviv.iot.algo.part1.lab1.python.model.Trolleybus import Trolleybus
+"""
+main func
+"""
+# pylint: disable = import-error
+from Python_Lab1_Term2.ua.lviv.iot.algo.part1.lab1.python.model.trolleybus import Trolleybus
 
 
 def main():
+    """
+    main func
+    :return:
+    """
     trolleybuses = [
         Trolleybus(44, 36, 122, 27, 3, 22),
         Trolleybus(11, 22, 110, 44, 11, 22),
         Trolleybus.get_instance(),
-        Trolleybus.get_instance()
-    ]
+        Trolleybus.get_instance(),
+]
 
     print(trolleybuses[::])
 
